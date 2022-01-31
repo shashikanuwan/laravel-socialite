f7580cbd42d01ca26a23457093a922a5b0560a8f
shashika nuwan <kumararanaweera1999@gmail.com>
2022-01-31 23:47:28 +0530

    fix error with fb login

--- app/Http/Controllers/SocialController.php
+++ app/Http/Controllers/SocialController.php
@@ -20,26 +20,38 @@ class SocialController extends Controller
         try {
 
             $user = Socialite::driver('facebook')->user();
-            $isUser = User::where('fb_id', $user->id)->first();
+            $isUser = User::where('email', $user->email)->first();
 
-            if ($isUser) {
-                Auth::login($isUser);
+            if ($isUser->fb_id !== $user->id) {
+                $isUser->update(['fb_id' => $user->id]);
+            }
+            $verifyUser = User::where('fb_id', $user->id)->first();
+            if ($verifyUser) {
+                Auth::login($verifyUser);
 
-                return redirect('/dashboard');
+                return redirect()
+                    ->route('dashboard');
             } else {
-                $password = $user->id . $user->email;
-                $createUser = User::create([
-                    'name' => $user->name,
-                    'email' => $user->email,
-                    'fb_id' => $user->id,
-                    'password' => Hash::make($password),
-                ]);
-
-                Auth::login($createUser);
-                return redirect('/dashboard');
+                return $this->register($user);
             }
         } catch (Exception $exception) {
             dd($exception->getMessage());
         }
+    }
+
+    private function register($user)
+    {
+        $password = $user->id . $user->email;
+        $createUser = User::create([
+            'name' => $user->name,
+            'email' => $user->email,
+            'fb_id' => $user->id,
+            'password' => Hash::make($password),
+        ]);
+
+        Auth::login($createUser);
+
+        return redirect()
+            ->route('dashboard');
     }
 }
